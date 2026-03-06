c94c984e0218fdea59fcb6e194d33120a1ae5cc1
Yuki <admin@yuki.sh>
2026-03-06 15:51:48 +0800

    feat(core): setup element-plus locale and element-hooks plugin

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import '@/styles/index.scss';
 
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import ElementHooks from 'element-hooks';
 import App from './App.vue';
 import pinia from './stores';
 import router from './router';
@@ -13,7 +15,12 @@ const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
-app.use(ElementPlus);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+app.use(ElementHooks, {
+  // https://element-hooks.js.org/guide/global-options#vue-plugin
+});
 
 await router.isReady();
 app.mount('#app');
